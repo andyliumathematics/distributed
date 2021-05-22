449b46cf76866e83fe5efa3ac44d31d9d2018454
liulei <liulei.bj@fang.com>
2021-05-22 15:15:32 +0800

    add

---
+++ service/service.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Start(ctx context.Context, serviceName, host, port string, RegisterHandlersFunc func()) (context.Context, error) {
+	RegisterHandlersFunc()
+	ctx = starService(ctx, serviceName, host, port)
+	return ctx, nil
+}
+
+func starService(ctx context.Context, serviceName string, host string, port string) context.Context {
+	ctx, cancel := context.WithCancel(ctx)
+	var srv http.Server
+	srv.Addr = ":" + port
+	go func() {
+		log.Println(srv.ListenAndServe())
+		cancel()
+	}()
+	go func() {
+		fmt.Printf("%v started. Press any key to stop.\n", serviceName)
+		var s string
+		fmt.Scanln(&s)
+		srv.Shutdown(ctx)
+		cancel()
+
+	}()
+	return ctx
+}
